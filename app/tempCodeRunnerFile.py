10efe85bd548baa48b75bbc6a996cb4088042f90
Otharv02 <162791201+Otharv02@users.noreply.github.com>
2025-04-30 19:50:52 +0530

    added upload/logs/setting button

---
+++ app/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+# Run this file for launching the application.
+# ui/gui       -> have the main gui
+# ui/login_gui -> have the login logic
+# ui/auth      -> have the username, password stored (for testing only)
+
+from ui.login_gui import launch_login_gui
+from tkinter import Tk
--- app/ui/gui.py
+++ app/ui/gui.py
@@ -2,6 +2,13 @@
 
 from tkinter import *
 
+def uploadAction(event=None):
+    from tkinter import filedialog
+    filename = filedialog.askopenfilename()
+    # print("Selected :", filename)
+
+
+
 
 
 def launch_gui(root,username):
@@ -15,7 +22,22 @@ def launch_gui(root,username):
     root.minsize(1050,700)
     root.maxsize(1050,700)
 
-    Label(root, text=f"Hello, {username}", bg="black", fg="white", font=("Helvetica", 12)).pack(pady=5)
+    Label(root, text=f"Hello, {username}", bg="black", fg="white", font=("Roboto", 12)).pack(pady=5)
 
+    # Action (Load File/History/Setting)
+    left_Frame = Frame(root, bg="lightblue")
+    left_Frame.place(x=0,y=0,relheight=1,relwidth=0.15)
+
+    #Upload Button 
+    upload_button = Button(left_Frame, text="Upload",width=15,command=uploadAction)
+    upload_button.pack(pady=10)
+
+    # log   
+    log_button = Button(left_Frame, text="History",width=15)
+    log_button.pack(pady=10)
+
+    # Setting
+    setting_button = Button(left_Frame, text="Upload",width=15)
+    setting_button.pack(pady=10)
 
 
